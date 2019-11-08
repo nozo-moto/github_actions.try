afe98bc3296884614e4aaec5c234eada8d5c1bfa
nozomi0966 <nozomotoitech@gmail.com>
2019-11-08 16:29:26 +0900

    pull req sampl

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/nozo-moto/github_actions.try
 
 go 1.13
 
-require github.com/google/uuid v1.1.1
+require (
+	github.com/google/uuid v1.1.1
+	github.com/k0kubun/colorstring v0.0.0-20150214042306-9440f1994b88 // indirect
+	github.com/k0kubun/pp v3.0.1+incompatible
+	github.com/mattn/go-colorable v0.1.4 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"sync"
 
 	"github.com/google/uuid"
+	"github.com/k0kubun/pp"
 )
 
 func getUUID() (string, error) {
@@ -14,6 +20,111 @@ func hoge() string {
 	return "hoge"
 }
 
+const (
+	testName = "test"
+)
+
 func main() {
-	fmt.Println("vim-go")
+	db := sync.Map{}
+	http.HandleFunc("/token", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "POST" {
+			e
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		var resJSON map[string]interface{}
+		err = json.Unmarshal(body, &resJSON)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		pp.Print(resJSON)
+		token, ok := resJSON["token"].(string)
+		if !ok {
+			log.Println("res json token is not good")
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
+		log.Println("res json")
+		pp.Print(resJSON)
+		log.Println("data ")
+		pp.Print(resJSON["data"])
+
+		d, ok := resJSON["data"].([]interface{})
+		if !ok {
+			log.Println("d json id is bad")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		data, ok := d[0].(map[string]interface{})
+		if !ok {
+			log.Println("data json id is bad")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		id, ok := data["id"].(string)
+		if !ok {
+			log.Println("res json id is bad")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.WriteHeader(http.StatusCreated)
+		fmt.Fprintf(w, "ok")
+		return
+	})
+
+	http.HandleFunc("/pay", func(w http.ResponseWriter, r *http.Request) {
+		query := r.URL.Query()
+		amount := query.Get("amount")
+		user := query.Get("user")
+
+		userInfo, ok := db.Load(user)
+		if !ok {
+			w.WriteHeader(http.StatusBadRequest)
+			fmt.Fprintf(w, "user is not exist")
+			return
+		}
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		pp.Print(resJSON)
+		d, ok := resJSON["data"].([]interface{})
+		if !ok {
+			log.Println("d json id is bad")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		data, ok := d[0].(map[string]interface{})
+		if !ok {
+			log.Println("data json id is bad")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		fmt.Fprintf(w, "you paied %f", data["amount"])
+		return
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
+
+	err := http.ListenAndServeTLS(":443", "server.crt", "server.key", nil)
+	if err != nil {
+		panic(err)
+	}
 }
